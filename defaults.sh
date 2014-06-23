c2088f86938412377e0c5cc900982221381094a0
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-06-23 17:49:17 +0200

    improve documentation and defaults

--- defaults.sh
+++ defaults.sh
@@ -1,6 +1,9 @@
 
-# pool and path where to create block devices
+# zfs pool and file system (existing) where to create block devices
 test -z "$EXTP_BLOCK" && EXTP_BLOCK=tmp500g/block
 
-# parameters for zfs create
+# parameters for zfs create, by default
+# -s	sparse
+# -b 4k	block size sutable for ext4 filesystem which ganeti uses
+# lz4	compression with low cpu operhead which always helps
 test -z "$EXTP_VOL_PARAM" && EXTP_VOL_PARAM="-s -b 4k -o compression=lz4"
