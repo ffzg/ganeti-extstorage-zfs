bf4e4316ffc6055c915b878b021809b39667de75
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-06-25 16:18:01 +0200

    gnt-backup import -t plain

--- t/gnt-backup-import.sh
+++ t/gnt-backup-import.sh
@@ -1,2 +1,5 @@
-# gnt-instance remove t-drbd
-gnt-backup import -t drbd -n arh-hw:box02 --no-name-check --no-ip-check t-drbd
+#gnt-instance remove t-drbd
+#gnt-backup import -t drbd -n arh-hw:box02 --no-name-check --no-ip-check t-drbd
+
+gnt-instance remove t-plain-remote # same MAC
+gnt-backup import -t plain -n arh-hw --no-name-check --no-ip-check t-plain
