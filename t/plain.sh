c5f479ecacafa0041fdc85e65de4d4d23d048b69
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-06-25 16:51:26 +0200

    creation of instace now saves ganeti:originstname

--- t/plain.sh
+++ t/plain.sh
@@ -2,6 +2,6 @@ ssh `gnt-cluster getmaster` \
   gnt-instance add -t plain --disk=0:size=10G \
 	-o debootstrap+default \
 	--no-name-check --no-ip-check \
-	-B maxmem=1G -n arh-hw t-plain
+	-B maxmem=512M -n arh-hw t-plain
 
 
