63ba7eecd1368d4fa69459b670129ea6fdc23685
Dobrica Pavlinusic <dpavlin@rot13.org>
2014-06-26 09:39:57 +0200

    copy originstname+adopt to adopt vol

--- t/adopt.sh
+++ t/adopt.sh
@@ -1,6 +1,7 @@
 ssh `gnt-cluster getmaster` \
   gnt-instance add -t plain --disk=0:adopt=adopt \
 	-o debootstrap+default \
+	--no-start \
 	--no-name-check --no-ip-check \
 	-B maxmem=512M -n arh-hw t-adopt
 
